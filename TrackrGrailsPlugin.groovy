3008ca92a2d62b48c69ba605fd4bf1efef0d40db
michaelvanvliet <info@michaelvanvliet.nl>
2012-02-14 15:17:49 +0000

    Version 0.6.3

--- TrackrGrailsPlugin.groovy
+++ TrackrGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class TrackrGrailsPlugin {
     // the plugin version
-    def version = "0.6.2"
+    def version = "0.6.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
